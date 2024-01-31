24ec8b69a05c405cfd432860b9a41fe09b1635d0
Mohitxroxx <mohitsinghroxx@gmail.com>
2024-01-31 22:41:59 +0530

    bug fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,7 +13,6 @@ const multer = require('multer')
 const FormData = require('form-data')
 const axios = require('axios')
 const upload = multer()
-const MongoStore = require('connect-mongo')
 
 // let permanent
 
@@ -27,8 +26,6 @@ app.use(
     session({
        secret: process.env.SESSION_SECRET,
        resave: false,
-       store: MongoStore.create({ 
-        mongoUrl: process.env.MONGO_URL}),
        saveUninitialized: false,
        cookie: {},
     })
@@ -89,21 +86,21 @@ app.post("/studentregister", async (req, res) => {
         const mailOptions = {
             from: SMTP_EMAIL,
             to: email,
-            subject: "Welcome To Health Bot",
+            subject: "Welcome To Referral Site",
             html: `<body>
             <body style="font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f7f7f7;">
             <table role="presentation" cellspacing="0" cellpadding="0"  width="600"
-            style="margin: 0 auto; background-color: #fff; padding: 20px; border-radius: 5px; box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.3);">p
+            style="margin: 0 auto; background-color: #fff; padding: 20px; border-radius: 5px; box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.3);">
             <tr>
             <td>
             <h3 style="color: #0838bc; font-size: 24px; text-align: center; margin-bottom: 10px;">Verification Mail</h3>
             <hr style="border: 1px solid #ccc; margin: 20px 0;">
             <h4 style="font-size: 20px; color: #333;">Hi there,</h4>
             <p style="font-size: 16px; color: #333; margin: 20px 0;">Here is the otp to confirm your mail ${otp}</p>
-            <p style="font-size: 16px; color: #333;">We are happy to have you.</p>
+            <p style="font-size: 16px; color: #333;">We are happy to have you as a user of our site.</p>
                         <div style="font-size: 16px; color: #333; margin-top: 20px; text-align: center;">
                         <h5 style="font-size: 18px;">Best Regards</h5>
-                        <h5 style="font-size: 18px;">Health Bot</h5>
+                        <h5 style="font-size: 18px;">Referral Site</h5>
                         </div>
                     </td>
                     </tr>
@@ -140,7 +137,12 @@ app.post("/studentverify", async (req, res) => {
             password: hashedPassword,
         })
         delete user.password
-        return res.status(200).json({ message: "Registered Successfully", user: user });
+        return res.status(200).json({ message: "Registered Successfully", user:{
+            name:user.name,
+            email:user.email,
+            username:user.username,
+            degree:user.degree
+        }})
     } else {
         return res.status(400).json({msg:"Invalid OTP cant register"})
     }
@@ -151,10 +153,10 @@ app.post("/refreelogin", async (req, res) => {
         const { username, password,rememberMe } = req.body
         const user = await refree.findOne({ username })
         if (!user)
-            return res.json({ msg: "Incorrect Username or Password", status: false })
+            return res.status(403).json({ msg: "Incorrect Username or Password", status: false })
         const isPasswordValid = await bcrypt.compare(password, user.password)
         if (!isPasswordValid)
-            return res.json({ msg: "Incorrect Username or Password", status: false })
+            return res.status(403).json({ msg: "Incorrect Username or Password", status: false })
         delete user.password
         const expiresIn = rememberMe ? '7d' : '2h';
         const token = jwt.sign({ id: user.id, username: user.username },  process.env.TOKEN_KEY, { expiresIn })
@@ -175,10 +177,10 @@ app.post("/refreeregister", async (req, res) => {
         const {username, email} = req.body
         const usernameCheck = await refree.findOne({ username })
         if (usernameCheck)
-            return res.status(404).json({ error: 'Username already used' })
+            return res.status(403).json({ error: 'Username already used' })
         const emailCheck = await refree.findOne({ email })
         if (emailCheck)
-            return res.status(404).json({ error: 'Email already used' })
+            return res.status(403).json({ error: 'Email already used' })
         // const otpreq={...req,body:{email}}
         // await otp(otpreq,res)
         // return res.status(200).json({ msg: 'Check Mail for further process' })
@@ -189,21 +191,21 @@ app.post("/refreeregister", async (req, res) => {
         const mailOptions = {
             from: SMTP_EMAIL,
             to: email,
-            subject: "Welcome To Health Bot",
+            subject: "Welcome To Referral Site",
             html: `<body>
             <body style="font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f7f7f7;">
             <table role="presentation" cellspacing="0" cellpadding="0"  width="600"
-            style="margin: 0 auto; background-color: #fff; padding: 20px; border-radius: 5px; box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.3);">p
+            style="margin: 0 auto; background-color: #fff; padding: 20px; border-radius: 5px; box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.3);">
             <tr>
             <td>
             <h3 style="color: #0838bc; font-size: 24px; text-align: center; margin-bottom: 10px;">Verification Mail</h3>
             <hr style="border: 1px solid #ccc; margin: 20px 0;">
             <h4 style="font-size: 20px; color: #333;">Hi there,</h4>
             <p style="font-size: 16px; color: #333; margin: 20px 0;">Here is the otp to confirm your mail ${otp}</p>
-            <p style="font-size: 16px; color: #333;">We are happy to have you.</p>
+            <p style="font-size: 16px; color: #333;">We are happy to have you as a refree of our site.</p>
                         <div style="font-size: 16px; color: #333; margin-top: 20px; text-align: center;">
                         <h5 style="font-size: 18px;">Best Regards</h5>
-                        <h5 style="font-size: 18px;">Health Bot</h5>
+                        <h5 style="font-size: 18px;">Referral Site</h5>
                         </div>
                     </td>
                     </tr>
@@ -228,7 +230,8 @@ app.post("/refreeregister", async (req, res) => {
 
 app.post("/refreeverify", async (req, res) => {
     const { name, username, email, company, password, otp } = req.body
-
+    if(!name||!email||!username||!company||!password)
+    return res.status(500).json({ message: "Make sure to enter all the fields correctly"})
     if (req.session.otp == otp) {
         const hashedPassword = await bcrypt.hash(password, 10)
         const user = await refree.create({
@@ -238,40 +241,18 @@ app.post("/refreeverify", async (req, res) => {
             company,
             password: hashedPassword,
         })
-        delete user.password
-        return res.status(200).json({ message: "Registered Successfully", user: user });
+        // delete user.password
+        return res.status(200).json({ message: "Registered Successfully", user:{
+            name:user.name,
+            email:user.email,
+            username:user.username,
+            company:user.company
+        } });
     } else {
         return res.status(400).json({msg:"Invalid OTP cant register"})
     }
 })
-
-
-app.post('/upload', upload.single('image'), async (req, res) => {
-    try {
-        const image = req.file;
-
-        const formData = new FormData();
-
-        formData.append('file', image.buffer, {
-            filename: image.originalname,
-            contentType: image.mimetype,
-        });
-        
-        const response = await axios.post('https://api.ocr.space/parse/image', formData, {
-            headers: {
-                ...formData.getHeaders(),
-                'apikey':process.env.key
-            },
-        })
-        
-        const text = response.data.ParsedResults[0].ParsedText;
-        res.json({ text });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'An error occurred while processing the image' });
-    }
-})
-
+ 
 
 app.get("/home", auth, (req, res) => {
     return res.status(200).json({ msg: 'User Logged in and Session is Active' })
