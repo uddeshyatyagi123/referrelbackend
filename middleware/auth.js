119e1cc9defac2b52100b6a015844db08ef39740
Mohitxroxx <mohitsinghroxx@gmail.com>
2024-02-21 15:45:05 +0530

    temporarily removed auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,15 +5,15 @@ const config = process.env;
 const verifyToken = (req, res, next) => {
     const token =req.cookies.jwt
 
-    if(!token){
-        return res.status(403).send("Session Expired Login again to post any data");
-    }
-    try{
-        const decoded = jwt.verify(token, config.TOKEN_KEY);
-        req.user = decoded;
-    } catch (err) {
-        return res.status(401).send("Error occured login again");
-    }
+    // if(!token){
+    //     return res.status(403).send("Session Expired Login again to post any data");
+    // }
+    // try{
+    //     const decoded = jwt.verify(token, config.TOKEN_KEY);
+    //     req.user = decoded;
+    // } catch (err) {
+    //     return res.status(401).send("Error occured login again");
+    // }
     return next();
 };
 
