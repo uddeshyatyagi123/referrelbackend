c5c78110542fa598ec555b58ace8920121e6a2a6
uddeshya tyagi <uddeshyatyagi123@gmail.com>
2025-06-11 02:50:02 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 const connectDB=async()=>{
     try {
-        const conn=await mongoose.connect(process.env.MONGO_URL,{
+        const conn=await mongoose.connect(`${process.env.MONGO_URL}`,{
         });
         console.log('MongoDB connected',conn.connection.host)
     } catch (err) {
